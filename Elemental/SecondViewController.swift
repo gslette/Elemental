9d98db0228d69e92cf994f94ba0f29376d438804
Gunnar Slette <gunnar@Gunnars-iMac.local>
2016-06-13 19:09:05 -0500

    Began initial work.
    Created basic mockup and boilerplate files. Need to spend more time
    learning UIKit.

---
+++ Elemental/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Elemental
+//
+//  Created by Gunnar on 3/28/16.
+//  Copyright © 2016 Gunnar Slette. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
